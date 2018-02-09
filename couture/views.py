cb70fdc46357cf3649c6447195d6613c8e53f82b
Mathilde <mathilde.mazo@gmail.com>
2018-02-09 23:34:01 +0100

    Install Bootstrap et avancée CSS

--- couture/views.py
+++ couture/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Pattern
 
 def pattern_list(request):
-    return render(request, 'couture/pattern_list.html', {})
+    patterns = Pattern.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+    return render(request, 'couture/pattern_list.html', {'patterns' : patterns})
